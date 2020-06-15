80cdc8d2937ef90dd7993e9093c2fbeae59d598b
Paul DAO <dao.van.paul@gmail.com>
2020-06-15 18:20:21 +0200

    css : product detail

--- src/components/ProductDetail.tsx
+++ src/components/ProductDetail.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent } from 'react';
-import { RouteComponentProps } from 'react-router-dom';
+import { RouteComponentProps, Link } from 'react-router-dom';
 import data from '../data';
+import './styles/product-detail.scss'
 
 type Params = {
     id: string;
@@ -12,7 +13,57 @@ const ProductDetail: FunctionComponent<RouteComponentProps<Params>> = ({match}) 
     
     return (
         <div>
-            {product?.name}
+
+            <div className="back">
+                <Link to="/">Back to Home</Link>
+            </div>
+
+            <div className="details">
+
+                <div className="details-image">
+                    <img src={product?.image} alt={product?.name}/>
+                </div>
+            
+                <div className="details-info">
+                    <ul>
+                        <li>
+                            <h4>{product?.name}</h4>
+                        </li>
+                        <li>
+                            {product?.rating} stars ({product?.nbReviews} reviews)
+                        </li>
+                        <li>
+                            Price : <b>$ {product?.price}</b>
+                        </li>
+                        <li>
+                            <p>Descrition :  </p>
+                        </li>
+                    </ul>
+                </div>
+
+                <div className="details-action">
+                    <ul>
+                        <li>
+                            Price : $ {product?.price}
+                        </li>
+                        <li>
+                            Number : 
+                            <select>
+                                <option value="">1</option>
+                                <option value="">2</option>
+                                <option value="">3</option>
+                                <option value="">4</option>
+                                <option value="">5</option>
+                            </select>
+                        </li>
+                        <li>
+                            <button className="details-action-btn primary">Add to cart</button>
+                        </li>
+                    </ul>
+                </div>
+                
+            </div>
+
         </div>
     )
 }
