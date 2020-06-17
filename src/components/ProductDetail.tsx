a39f76f303273179f4abc6f3389ae9e7b0f7c4cb
Paul DAO <dao.van.paul@gmail.com>
2020-06-17 19:54:17 +0200

    add handle number traveller with number of trip

--- src/components/ProductDetail.tsx
+++ src/components/ProductDetail.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import { Link, RouteComponentProps } from 'react-router-dom';
 import useProductDetails from '../hooks/productDetails.hook';
 import './styles/product-detail.scss';
@@ -9,10 +9,24 @@ type Params = {
 }
 
 const ProductDetail: FunctionComponent<RouteComponentProps<Params>> = ({match}) => {
+
+    const [qty, setQty] = useState(1);
     
     const productReducer = useProductDetails(+match.params.id);
 
     const product = productReducer.product;
+
+    const numberTraverllers = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setQty(+e.target.value);
+    }
+
+    const fewQuatity = (qty: number) => {
+        const array = []
+        for (let i = 0; i < qty; i++) {
+            array.push(i);
+        }
+        return array;
+    }
     
     return (
 
@@ -57,19 +71,57 @@ const ProductDetail: FunctionComponent<RouteComponentProps<Params>> = ({match}) 
                         <li>
                             Price : $ {product?.price}
                         </li>
-                        <li>
-                            Number : 
-                            <select>
-                                <option value="">1</option>
-                                <option value="">2</option>
-                                <option value="">3</option>
-                                <option value="">4</option>
-                                <option value="">5</option>
-                            </select>
-                        </li>
-                        <li>
-                            <button className="details-action-btn primary">Add to cart</button>
-                        </li>
+                        {
+                            product.quantity > 10 ? 
+                                <li>
+                                    Trip available
+                                </li>
+                            : (
+                            product.quantity > 1 &&
+                                <li>
+                                Only {product.quantity} trips left !   
+                                </li>
+                            )
+                        }
+
+
+                        { product.quantity > 10 ?
+                            <li>
+                                Traveller's number: 
+                                <select value={qty} onChange={numberTraverllers}>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                    <option value="6">6</option>
+                                </select>
+                            </li>
+                            : (
+                                product.quantity > 1 &&
+                            <li>
+                                Traveller's number: 
+
+                                    <select value={qty} onChange={numberTraverllers}>
+                                    { fewQuatity(product.quantity).map(x => (
+                                        <option key={x+1} value={x+1}>{x + 1}</option>
+                                        ))}
+                                </select>
+                            </li>
+                            )
+                        }
+                            
+                        
+                            
+                        {
+                            product.quantity > 0 ?
+                                <li>
+                                    <button className="details-action-btn primary">Add to cart</button>
+                                </li>
+                            :   
+                            <li>Sold out</li>
+                        }
+                        
                     </ul>
                 </div>
                 
--- src/models/product.ts
+++ src/models/product.ts
@@ -6,4 +6,5 @@ export interface Product {
     price:     number;
     rating:    number;
     nbReviews: number;
+    quantity:  number;
 }
